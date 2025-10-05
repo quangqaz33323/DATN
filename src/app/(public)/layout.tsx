9dcbfe5e028b84283baea09b54ab52c62806e1a1
quangqaz33323 <quangaaq@gmail.com>
2025-10-05 19:35:42 +0700

    create project folder

---
+++ src/app/(public)/layout.tsx
@@ -0,0 +1,8 @@
+
+
+export default function PublicLayout() {
+
+    return (
+      <div>Public Layout</div>
+    );
+}
---
+++ src/app/store/layout.tsx
@@ -0,0 +1,8 @@
+
+
+export default function StoreLayout() {
+
+    return (
+      <div>Store Layout</div>
+    );
+}
