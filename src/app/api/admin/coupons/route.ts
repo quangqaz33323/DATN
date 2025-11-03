081928b45d0e220a5b4a2e7f2cbbc1b49e46667e
quangqaz33323 <quangaaq@gmail.com>
2025-11-03 20:24:00 +0700

    Revert "feat: add coupon expiration handling and integrate with Inngest"

--- src/app/api/admin/coupons/route.ts
+++ src/app/api/admin/coupons/route.ts
@@ -2,7 +2,6 @@ import authAdmin from "@/app/middlewares/authAdmin";
 import { getAuth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
-import { inngest } from "@/inngest/client";
 
 export async function POST(request: NextRequest) {
   try {
@@ -22,19 +21,9 @@ export async function POST(request: NextRequest) {
 
     coupon.code = coupon.code.toUpperCase();
 
-    await prisma.coupon
-      .create({
-        data: coupon,
-      })
-      .then(async (coupon) => {
-        await inngest.send({
-          name: "app/coupon.expired",
-          data: {
-            code: coupon.code,
-            expires_at: coupon.expiresAt,
-          },
-        });
-      });
+    await prisma.coupon.create({
+      data: coupon,
+    });
 
     return NextResponse.json("Coupon created successfully", { status: 200 });
   } catch (error) {
